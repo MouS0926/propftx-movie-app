875eb51c38472a60b8d77d79bfd2757c11580b1a
MouS0926 <moupriya2609@gmail.com>
2024-02-15 18:29:28 +0530

    add-edit-delete

--- Backend/models/movieModel.js
+++ Backend/models/movieModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
     title: String,
-      
+    image: String,
     director:  String,
     genre:  String,
     
--- Backend/routes/movieRoute.js
+++ Backend/routes/movieRoute.js
@@ -8,21 +8,56 @@ const movieRoute=express.Router()
 
 movieRoute.post("/add",auth,async(req,res)=>{
     try {
-        // Extract movie details from request body
-        //const { title, director, genre, year } = req.body;
+        
+    const newMovie = new movieModel(req.body);
+    const savedMovie = await newMovie.save();
+    res.status(201).json(savedMovie);
 
-        // Create a new movie object
-        const newMovie = new movieModel(req.body);
-
-        // Save the movie to the database
-        const savedMovie = await newMovie.save();
-
-        // Respond with the saved movie
-        res.status(201).json(savedMovie);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
     }
 })
+
+movieRoute.patch("/update/:id", auth, async (req, res) => {
+    try {
+        const movieId = req.params.id;
+        const updates = req.body;
+    const movie=await movieModel.findOne({_id:movieId})
+    if(movie.createdBy!==req.body.createdBy)
+    {
+        return res.status(500).send({"meassage":"You are not authorised"});
+    }
+
+        const updatedMovie = await movieModel.findByIdAndUpdate(movieId, updates, { new: true });
+        if (!updatedMovie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+        res.status(200).json(updatedMovie);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+movieRoute.delete("/delete/:id", auth, async (req, res) => {
+    try {
+        const movieId = req.params.id;
+
+        const movie=await movieModel.findOne({_id:movieId})
+        if(movie.createdBy!==req.body.createdBy)
+        {
+            return res.status(500).send({"meassage":"You are not authorised"});
+        }
+        const deletedMovie = await movieModel.findByIdAndDelete(movieId);
+        if (!deletedMovie) {
+            return res.status(404).send({ message: "Movie not found" });
+        }
+        res.json({ message: "Movie deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal server error" });
+    }
+});
 
 module.exports={movieRoute}
